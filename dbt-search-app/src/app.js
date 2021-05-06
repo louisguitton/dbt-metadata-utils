d5f5638e2d0c5d65c3d05a622502c8c93c60ffda
Louis Guitton <louisguitton93@gmail.com>
2021-05-06 21:44:41 +0200

    Add github action to deploy github pages

--- dbt-search-app/src/app.js
+++ dbt-search-app/src/app.js
@@ -1,49 +1,61 @@
-require('dotenv').config({ path: '../.env' })
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import {
+  searchBox,
+  hits,
+  stats,
+  configure,
+  pagination,
+  refinementList,
+  clearRefinements,
+} from 'instantsearch.js/es/widgets';
 
 const searchClient = algoliasearch(
-    process.env.ALGOLIA_APP_ID,
-    process.env.ALGOLIA_SEARCH_ONLY_API_KEY
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_SEARCH_ONLY_API_KEY
 );
 
 const search = instantsearch({
-    indexName: process.env.ALGOLIA_INDEX_NAME || 'dbt_nodes',
-    searchClient,
+  indexName: process.env.ALGOLIA_INDEX_NAME || 'dbt_nodes',
+  searchClient,
 });
 
 // https://www.algolia.com/doc/guides/building-search-ui/widgets/showcase/js/
 search.addWidgets([
-    instantsearch.widgets.searchBox({
-        container: '#searchbox',
-        placeholder: 'Search for a dataset',
-    }),
-    instantsearch.widgets.stats({
-        container: '#stats',
-    }),
-    instantsearch.widgets.hits({
-        container: '#hits',
-        templates: {
-            // https://www.algolia.com/doc/api-reference/widgets/hits/js/#widget-param-item
-            // https://www.algolia.com/doc/guides/building-search-ui/widgets/customize-an-existing-widget/js/#example-using-a-function
-            // https://www.algolia.com/doc/guides/building-search-ui/ui-and-ux-patterns/highlighting-snippeting/js/#response-information
-            item(hit) {
-                return `<article>
+  searchBox({
+    container: '#searchbox',
+    placeholder: 'Search for a dataset',
+  }),
+  stats({
+    container: '#stats',
+  }),
+  hits({
+    container: '#hits',
+    templates: {
+      // https://www.algolia.com/doc/api-reference/widgets/hits/js/#widget-param-item
+      // https://www.algolia.com/doc/guides/building-search-ui/widgets/customize-an-existing-widget/js/#example-using-a-function
+      // https://www.algolia.com/doc/guides/building-search-ui/ui-and-ux-patterns/highlighting-snippeting/js/#response-information
+      item(hit) {
+        return `<article>
                 <h2 class="hit-name">${hit._highlightResult.name.value}</h2>
-                <p class="hit-description">${hit._highlightResult.description.value}</p>
+                <p class="hit-description">${
+                  hit._highlightResult.description.value
+                }</p>
                 <dl class="hit-metadata">
                     <dt>folder:</dt>
                     <dd>${hit._highlightResult.folder.value}</dd>
                     <dt>sources:</dt>
                     <dd><ul>${(hit._highlightResult.sources || [])
-                        .map(({ value }) => `<li>${value}</li>`)
-                        .join('')}</ul></dd>
+                      .map(({ value }) => `<li>${value}</li>`)
+                      .join('')}</ul></dd>
                     <dt>type:</dt>
                     <dd>${hit.resource_type}<dd>
                     <dt>materialization:</dt>
                     <dd>${hit.materialized}</dd>
                     <dt>loaders:</dt>
                     <dd><ul>${hit.loaders
-                        .map((loader) => `<li>${loader}</li>`)
-                        .join('')}</ul></dd>
+                      .map(loader => `<li>${loader}</li>`)
+                      .join('')}</ul></dd>
                     <dt>centrality:</dt>
                     <dd>${hit.degree_centrality}</dd>
                     <dt>owner:</dt>
@@ -51,44 +63,46 @@ search.addWidgets([
                     <dt>created_at:</dt>
                     <dd>${new Date(hit.created_at * 1000).toDateString()}</dd>
                     <dt>last_modified_at:</dt>
-                    <dd>${new Date(hit.last_modified_at * 1000).toDateString()}</dd>
+                    <dd>${new Date(
+                      hit.last_modified_at * 1000
+                    ).toDateString()}</dd>
                 </dl>
                 </article>`;
-            },
-        },
-    }),
-    instantsearch.widgets.pagination({
-        container: '#pagination',
-    }),
-    instantsearch.widgets.clearRefinements({
-        container: '#clear-refinements',
-    }),
-    // https://www.algolia.com/doc/api-reference/widgets/refinement-list/js/
-    instantsearch.widgets.refinementList({
-        container: '#sources-list',
-        attribute: 'sources',
-        searchable: true,
-    }),
-    instantsearch.widgets.refinementList({
-        container: '#folder-list',
-        attribute: 'folder',
-        searchable: true,
-    }),
-    instantsearch.widgets.refinementList({
-        container: '#materialized-list',
-        attribute: 'materialized',
-    }),
-    instantsearch.widgets.refinementList({
-        container: '#resource_type-list',
-        attribute: 'resource_type',
-    }),
-    instantsearch.widgets.refinementList({
-        container: '#loaders-list',
-        attribute: 'loaders',
-    }),
-    instantsearch.widgets.configure({
-        hitsPerPage: 5,
-    })
+      },
+    },
+  }),
+  pagination({
+    container: '#pagination',
+  }),
+  clearRefinements({
+    container: '#clear-refinements',
+  }),
+  // https://www.algolia.com/doc/api-reference/widgets/refinement-list/js/
+  refinementList({
+    container: '#sources-list',
+    attribute: 'sources',
+    searchable: true,
+  }),
+  refinementList({
+    container: '#folder-list',
+    attribute: 'folder',
+    searchable: true,
+  }),
+  refinementList({
+    container: '#materialized-list',
+    attribute: 'materialized',
+  }),
+  refinementList({
+    container: '#resource_type-list',
+    attribute: 'resource_type',
+  }),
+  refinementList({
+    container: '#loaders-list',
+    attribute: 'loaders',
+  }),
+  configure({
+    hitsPerPage: 5,
+  }),
 ]);
 
 search.start();
